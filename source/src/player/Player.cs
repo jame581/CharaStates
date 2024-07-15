4e6be0ebe841a9eeb25bf617042d79d5d20251fc
Jan Mesarč <jame_581@windowslive.com>
2024-07-15 23:56:39 +0200

    Refactor player movement logic and add animations

--- source/src/player/Player.cs
+++ source/src/player/Player.cs
@@ -9,31 +9,57 @@ public partial class Player : CharacterBody2D
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+	private AnimatedSprite2D animatedSprite;
+
+	public override void _Ready()
+	{
+		base._Ready();
+
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		if (animatedSprite == null)
+			GD.PrintErr("AnimatedSprite2D node not found.");
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
-
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		float direction = Input.GetAxis("left", "right");
+		if (direction != 0)
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = direction * Speed;
+			animatedSprite.Play("walk");
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			animatedSprite.Play("idle");
 		}
 
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity.Y += gravity * (float)delta;
+			animatedSprite.Play("jump");
+		}
+
+
 		Velocity = velocity;
+		FlipSpriteByDirection();
 		MoveAndSlide();
+	}
+
+	private void FlipSpriteByDirection()
+	{
+		if (Velocity.X < 0)
+			animatedSprite.Scale = new Vector2(-1, 1);
+		else if (Velocity.X > 0)
+			animatedSprite.Scale = new Vector2(1, 1);
 	}
 }
